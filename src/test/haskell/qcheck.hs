7ca8ee5141183d8b43ab3ce36a3a59d368424435
reeyuk <sofiane.laouini@gmail.com>
2015-10-17 15:45:24 +0200

    move quick check file.

---
+++ src/test/haskell/qcheck.hs
@@ -0,0 +1,16 @@
+import Test.QuickCheck
+
+data Move = LEFT | RIGHT | FORWARD deriving (Eq, Ord, Show, Bounded, Enum)
+
+instance Arbitrary Move where
+  arbitrary = do
+    n <- choose (0,2) :: Gen Int
+    return $ case n of
+      0 -> LEFT
+      1 -> RIGHT
+      2 -> FORWARD
+
+main :: IO [Move] -- todo add some quick check tests
+main = do
+    let moves = sample' arbitrary
+
